b3c770cd970f6483d1691af1f306cd0dd39dba96
Mark Strendin <mark.strendin@lskysd.ca>
2021-07-21 14:16:03 -0600

    add poller project, fix spelling mistake in sln file

---
+++ src/EnvironmentSensorDashboard.Poller/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EnvironmentSensorDashboard.Poller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
