084c9f289d4b6646a3a4bc0d8a28292caffa3ba8
Mark Strendin <mark.strendin@lskysd.ca>
2021-07-21 14:14:24 -0600

    Initial commit - added some default asp.net projects to start

---
+++ src/EnvironmentSensorDashboard.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace EnvironmentSensorDashboard.Data
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/EnvironmentSensorDashboard.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace EnvironmentsensorDashboard.Domain
+{
+    public class Class1
+    {
+    }
+}
